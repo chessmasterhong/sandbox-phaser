76a478992fc0b6b8c911e3786a5d7abc0974b0d7
Kevin Chan <chessmasterhong@yahoo.com>
2014-06-25 19:01:08 -0400

    Added state loading. Added main game state.

--- scripts/game.js
+++ scripts/game.js
@@ -1,12 +1,19 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-container', {
-    preload: preload,
-    create: create
-});
+var MainGame = function(game) {};
 
-function preload() {
-    game.load.image('tiles', 'media/tilesets/mountain_landscape_23.png');
+MainGame.prototype = {
+    preload: function() {
+        this.game.load.image('tiles', 'media/tilesets/mountain_landscape_23.png');
+    },
+
+    create: function() {
+        this.game.add.sprite(0, 0, 'tiles');
+    }
 }
 
-function create() {
-    game.add.sprite(0, 0, 'tiles');
-}
+//==============================================================================
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-container');
+
+game.state.add('MainGame', MainGame);
+
+game.state.start('MainGame');
