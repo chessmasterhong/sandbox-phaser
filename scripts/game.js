2b684bb07f808fdda9bcf4b70ea78e3dc1d8de63
Kevin Chan <chessmasterhong@yahoo.com>
2014-06-25 18:48:05 -0400

    Added loading basic image.

--- scripts/game.js
+++ scripts/game.js
@@ -1,1 +1,12 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-container');
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-container', {
+    preload: preload,
+    create: create
+});
+
+function preload() {
+    game.load.image('tiles', 'media/tilesets/mountain_landscape_23.png');
+}
+
+function create() {
+    game.add.sprite(0, 0, 'tiles');
+}
