626ea994da61a28bf01035fd11935f5ba12964c2
Kevin Chan <chessmasterhong@yahoo.com>
2014-06-25 19:09:20 -0400

    Added load complete listener to preloader.

--- scripts/game.js
+++ scripts/game.js
@@ -1,14 +1,26 @@
-var Preloader = function(game) {};
+'use strict';
+
+var Preloader = function(game) {
+    this.ready = false;
+};
 
 Preloader.prototype = {
     preload: function() {
+        this.game.load.onLoadComplete.addOnce(this.onLoadComplete, this);
+
         this.game.load.image('tiles', 'media/tilesets/mountain_landscape_23.png');
     },
 
     update: function() {
-        this.game.state.start('MainGame');
+        if(this.ready) {
+            this.game.state.start('MainGame');
+        }
+    },
+
+    onLoadComplete: function() {
+        this.ready = true;
     }
-}
+};
 
 //==============================================================================
 
@@ -18,7 +30,7 @@ MainGame.prototype = {
     create: function() {
         this.game.add.sprite(0, 0, 'tiles');
     }
-}
+};
 
 //==============================================================================
 
