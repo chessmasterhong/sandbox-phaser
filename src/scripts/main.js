78a67cf7af629cd387ff1cc4495c61740910941a
Kevin Chan <chessmasterhong@yahoo.com>
2014-06-27 09:29:28 -0400

    Reorganizing main requires list. Updated .gitignore.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,30 +1,39 @@
+'use strict';
+
 requirejs.config({
     baseUrl: 'scripts',
     paths: {
-        phaser: 'bower_components/phaser/index'
-    },
-    shim: {
-        phaser: { exports: 'Phaser' }
+        Phaser: 'bower_components/phaser/index',
+
+        Boot: 'game/states/boot',
+        Preloader: 'game/states/preloader',
+        MainGame: 'game/states/maingame'
     }
 });
 
+define([
+    'require',
+    'Phaser',
+    'Boot',
+    'Preloader',
+    'MainGame'
+], function(require) {
+    var Phaser = require('Phaser'),
+        Boot = require('Boot'),
+        Preloader = require('Preloader'),
+        MainGame = require('MainGame');
 
-require([
-        'phaser',
-        'game/states/boot',
-        'game/states/preloader',
-        'game/states/maingame'
-    ], function(
-        Phaser, Boot, Preloader, MainGame
-    ) {
-        var game = new Phaser.Game(800, 600, Phaser.CANVAS);
+    var game = new Phaser.Game(800, 600, Phaser.CANVAS);
 
-        game.state.add('Boot', Boot);
-        game.state.add('Preloader', Preloader);
-        game.state.add('MainGame', MainGame);
+    game.state.add('Boot', Boot);
+    game.state.add('Preloader', Preloader);
+    game.state.add('MainGame', MainGame);
 
-        game.state.start('Boot');
+    game.state.start('Boot');
 
-        return game;
+    window.game = game || {};
+    console.log(window.game.state.states);
+
+    return game;
 });
 
